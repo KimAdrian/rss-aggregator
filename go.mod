0b9729582039cf87c6ab903486cffa061a3b2b24
KimAdrian <adriankimutai5@gmail.com>
2023-05-26 23:56:24 +0300

    Setup .env file and implement basic server

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/KimAdrian/rss-aggregator
 
 go 1.19
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	//Load .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalln("Could not load .env file")
+		return
+	}
+
+	port := os.Getenv("PORT")
+	log.Printf("PORT: %s\n", port)
+
+	//setup chi router object
+	router := chi.NewRouter()
+
+	httpServer := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	//Start server and handle http requests
+	log.Printf("Server starting on port: %v", port)
+	httpError := httpServer.ListenAndServe()
+	if httpError != nil {
+		log.Println(httpError)
+		return
+	}
 }
